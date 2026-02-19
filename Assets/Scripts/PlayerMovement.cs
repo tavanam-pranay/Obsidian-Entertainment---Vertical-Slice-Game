0c48feae3d1cbc3ae5162414059f2fa8862fecca
Pranay Tavanam <pranay.271@gmail.com>
2026-02-19 16:55:03 -0500

    Basic Player Movement + Arm
    added temporary floor object, player capsule object with WASD movement and player arm that extends with Q keypress

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D player;
+
+    private Vector2 movement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        player.MovePosition(player.position + movement * moveSpeed * Time.deltaTime);
+    }
+}
