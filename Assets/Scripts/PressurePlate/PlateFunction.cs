88ecec179d05032e1d592e8dbc2ba3120982c5ca
Pranay Tavanam <pranay.271@gmail.com>
2026-03-10 11:22:22 -0400

    last minute touch up

--- Assets/Scripts/PressurePlate/PlateFunction.cs
+++ Assets/Scripts/PressurePlate/PlateFunction.cs
@@ -8,6 +8,9 @@ public class PlateFunction : MonoBehaviour
     public GameObject door;
     public List<Collider2D> objectsOnTop = new List<Collider2D>();
 
+    // attach player's magnet collider as parameter to disqualify magnet hitbox from activating plate
+    public Collider2D beamCollider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,33 +25,39 @@ public class PlateFunction : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!objectsOnTop.Contains(other))
+        if (other != beamCollider)
         {
-            objectsOnTop.Add(other);
-        }
+            if (!objectsOnTop.Contains(other))
+            {
+                objectsOnTop.Add(other);
+            }
 
-        //trigger the press with the first object on top
-        if (objectsOnTop.Count == 1) 
-        {
-            plateAnim.enabled = true;
-            doorOpen();
-            Debug.Log("plate pressed!");
+            //trigger the press with the first object on top
+            if (objectsOnTop.Count == 1)
+            {
+                plateAnim.enabled = true;
+                doorOpen();
+                Debug.Log("plate pressed!");
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (objectsOnTop.Contains(other))
+        if (other != beamCollider)
         {
-            objectsOnTop.Remove(other);
-        }
+            if (objectsOnTop.Contains(other))
+            {
+                objectsOnTop.Remove(other);
+            }
 
-        //only release plate if there's nothing on top
-        if (objectsOnTop.Count == 0)
-        {
-            doorClose();
-            Debug.Log("plate released!");
-            //animate plate release
+            //only release plate if there's nothing on top
+            if (objectsOnTop.Count == 0)
+            {
+                doorClose();
+                Debug.Log("plate released!");
+                //animate plate release
+            }
         }
     }
 
