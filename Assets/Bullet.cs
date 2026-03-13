7bfed1d72cab0e5835327098388581cefd7c8439
Onion <saschalalonde@gmail.com>
2026-03-12 22:02:21 -0400

    Fixed bullets

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -23,4 +23,13 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.up * speed; // Move the bullet in the direction it's facing
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) // If the bullet hits the player, destroy the bullet and damage the player.
+        {
+            Debug.Log("Bullet hit the player!");
+        }
+        Destroy(gameObject);
+
+    }
 }
