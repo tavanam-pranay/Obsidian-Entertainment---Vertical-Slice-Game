78070d377b186213713e9e9016149166651de9fc
Pranay Tavanam <pranay.271@gmail.com>
2026-04-07 16:30:57 -0400

    unused code removal + server weight increase

--- Assets/Scripts/Plates and Buttons/WallButtonFunction.cs
+++ Assets/Scripts/Plates and Buttons/WallButtonFunction.cs
@@ -4,32 +4,18 @@ using UnityEngine;
 
 public class WallButtonFunction : MonoBehaviour
 {
-    //public GameObject player;
-    //public Collider2D beamCollider;
     public Animator plateAnim;
     public bool inRange;
     public bool clicked;
 
-    void Start()
-    {
-
-        //if (beamCollider == null)
-        //{
-        //    Debug.Log("Beam collider not assigned");
-        //    beamCollider = player.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(2).GetComponent<Collider2D>(); //Pranay's original code, unsure if works with new player prefab
-        //}
-    }
-
     public void OnTriggerEnter2D(Collider2D hand)
     {
-        //if (hand != beamCollider)
-            inRange = true;
+        inRange = true;
     }
 
     public void OnTriggerExit2D(Collider2D hand)
     {
-        //if (hand != beamCollider)
-            inRange = false;
+        inRange = false;
     }
 
     void Update()
